83037a68a91ac0b3f03e6f7974a893008cd93c6c
herve <htchikladze@ippon.fr>
2023-02-04 21:53:51 +0100

    ajout des promesses

--- vegetables.js
+++ vegetables.js
@@ -14,15 +14,27 @@ function getAllVegetables() {
       ],
       "promises": [
         {
-          "text": "",
+          "text": "Rrrrh ! Le saligot qui va passer après moi me doit de la thune ! Mange le tiens ! Ça lui fera les graines",
           "sound": "",
           "cond": {
-            days: 10,
-            action: 0,
-            vegetables: [1]
+            days: 1,
+            action: 2,
+            vegetables: [1, 2, 3, 4],
+            steps: 1
           },
-          "reward": 20,
+          "reward": 10,
           "punishment": -10
+        },
+        {
+          "text": "Eeh euuh toi là, ouai c’est à toi qu’je cause ! File de la thune à 2 de mes copains, on te revaudra ça t’inquiète ahah.. Tu as 5 jours !",
+          "sound": "",
+          "cond": {
+            days: 5,
+            action: 0,
+            vegetables: [1, 1]
+          },
+          "reward": 30,
+          "punishment": -15
         }
       ]
     },
@@ -40,15 +52,28 @@ function getAllVegetables() {
       ],
       "promises": [
         {
-          "text": "",
+          "text": "J’suis dans la sauce, je dois de la thune à la prochaine banane que tu vas voir. Si tu le vois, vends le, je ferais n’importe quoi. fait vite, tu as 3 jours !",
           "sound": "",
           "cond": {
-            days: 10,
-            action: 0,
-            vegetables: [1]
+            days: 3,
+            action: 1,
+            vegetables: [1],
+            step: 1
           },
-          "reward": 20,
-          "punishment": -10
+          "reward": 15,
+          "punishment": -20
+        },
+        {
+          "text": "Je pense que le clan des bananes prépare un sale coup, débarasse toi d’eux avant la fin de la semaine !",
+          "sound": "",
+          "cond": {
+            days: 7,
+            action: [1, 2],
+            vegetables: [1],
+            step: 1
+          },
+          "reward": 35,
+          "punishment": -30
         }
       ]
     },
@@ -66,14 +91,25 @@ function getAllVegetables() {
       ],
       "promises": [
         {
-          "text": "",
+          "text": "Ola seniorita, rend moi un service, mange le prochain comme moi, ça lui fera plaisir, je te laisse 72h",
           "sound": "",
           "cond": {
-            days: 10,
-            action: 0,
-            vegetables: [1]
+            days: 3,
+            action: 2,
+            vegetables: [3]
           },
           "reward": 20,
+          "punishment": -20
+        },
+        {
+          "text": "Ehh psss psss, tu aurais pas quelques pesos pour le prochain, je suis d’humour généreuse aujourd’hui",
+          "sound": "",
+          "cond": {
+            days: 1,
+            action: [0],
+            vegetables: [1, 2, 3, 4]
+          },
+          "reward": 10,
           "punishment": -10
         }
       ]
